63e692b6a922ccfe2d41c84f1638e24e46138aeb
stephenmdevine <stephenmdevine@gmail.com>
2023-09-18 00:27:24 -0500

    Exercises for Arrays parts 1-6 complete

--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,43 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
 
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+let table = [];
+table.push(element1,element2,element26);
+console.log(table);
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
+console.log(table[1]);
+console.log(table[1][1]);
 
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(table[0][2]);
+console.log(table[1][0]);
+console.log(table[2][1]);
 
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+let student1 = ['A', 'A', 'B', 'A', 'B'];
+let student2 = ['B', 'B', 'A', 'C', 'D'];
+let student3 = ['A', 'B', 'C', 'D', 'F'];
+
+let student4 = ['B', 'C', 'B', 'A', 'A'];
+let student5 = ['A', 'A', 'A', 'A', 'A'];
+let student6 = ['D', 'D', 'F', 'C', 'F'];
+
+let student7 = ['A', 'C', 'D', 'C', 'B'];
+let student8 = ['B', 'C', 'B', 'A', 'C'];
+let student9 = ['C', 'B', 'A', 'A', 'C'];
+
+let class1 = [student1, student2, student3];
+let class2 = [student4, student5, student6];
+let class3 = [student7, student8, student9];
+
+let instructorStephen = [class1, class2, class3];
+console.log(instructorStephen);
+console.log(instructorStephen[0])
+console.log(instructorStephen[1][0]);
+console.log(instructorStephen[2][0][1]);
