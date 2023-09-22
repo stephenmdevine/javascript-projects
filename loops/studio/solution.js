b37f7da7564a33bfa4c008d63fed5740540f4efe
stephenmdevine <stephenmdevine@gmail.com>
2023-09-21 20:23:57 -0500

    Added nested for loop

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -15,16 +15,18 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-  for (let i = 0; i < numMeals; i++) {
-    let tempMeal = [];
-    tempMeal = [ protein[i], grains[i], veggies[i], beverages[i], desserts[i] ];
-    meals.push(tempMeal);
-  }
   // for (let i = 0; i < numMeals; i++) {
-  //   for (let j = 0; j < pantry.length; j++){
-  //     meals.push(pantry[j][i]);
-  //   }
+  //   let tempMeal = [];
+  //   tempMeal = [ protein[i], grains[i], veggies[i], beverages[i], desserts[i] ];
+  //   meals.push(tempMeal);
   // }
+  for (let i = 0; i < numMeals; i++) {
+    let meal = [];
+    for (let j = 0; j < pantry.length; j++){
+      meal.push(pantry[j][i]);
+    }
+    meals.push(meal);
+  }
 
   return meals;
 }
@@ -64,16 +66,16 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  console.log(meals);
+  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  // console.log(meals);
   
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
