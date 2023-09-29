01ac6492e60954ba686b5ac15b96470a1a4bac3e
stephenmdevine <stephenmdevine@gmail.com>
2023-09-28 20:01:03 -0500

    Small removal

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -1,6 +1,6 @@
 //1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
 function lowestNumberPop(numArray) {
-    let low = Number(numArray[0]);
+    let low = numArray[0];
     for (let i = 0; i < numArray.length; i++) {
         if (numArray[i] < low) {
             low = numArray[i];
@@ -25,4 +25,4 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(resortedArray(nums3));
+console.log(resortedArray(nums1));
