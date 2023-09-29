8966d01015b17f7756ca0e28cae75313a313f0d9
stephenmdevine <stephenmdevine@gmail.com>
2023-09-28 19:55:47 -0500

    Completed part 1 of the studio

---
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -0,0 +1,28 @@
+//1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
+function lowestNumberPop(numArray) {
+    let low = Number(numArray[0]);
+    for (let i = 0; i < numArray.length; i++) {
+        if (numArray[i] < low) {
+            low = numArray[i];
+        }
+    }
+    numArray.splice(numArray.indexOf(low), 1);
+    return low;
+}
+
+function resortedArray(numArray) {
+    let newArray = [];
+    while (numArray.length > 0) {
+        newArray.push(lowestNumberPop(numArray));
+    }
+    return newArray;
+}
+
+//Sample arrays for testing:
+let nums1 = [5, 10, 2, 42];
+let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+//Using one of the test arrays as the argument, call your function inside the console.log statement below.
+
+console.log(resortedArray(nums3));
