1532bab9550e0f29054ba53cbced2f97faeb37a8
stephenmdevine <stephenmdevine@gmail.com>
2023-10-29 22:24:53 -0500

    Prepped website for Liftoff!

---
+++ dom-and-events/exercises/script.js
@@ -0,0 +1,25 @@
+function init () {
+    const missionAbort = document.getElementById("abortMission");
+    const button = document.getElementById("liftoffButton");
+    const paragraph = document.getElementById("statusReport");
+
+    // Put your code for the exercises here.
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston! We have liftoff!";
+     });
+     missionAbort.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "red";
+     });
+     missionAbort.addEventListener("mouseout", event => {
+        event.target.style.backgroundColor = "";
+     });
+     missionAbort.addEventListener("click", event => {
+        let confirm = window.confirm("Are you sure you want to abort the mission?");
+        if(confirm) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+     });
+    
+}
+
+window.addEventListener("load", init);
