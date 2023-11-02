45f6c6efa18509c633164a4ade89223f02602081
stephenmdevine <stephenmdevine@gmail.com>
2023-11-01 23:38:58 -0500

    Exercise Bonus Mission complete

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,15 @@
 //Code Your Solution Below
+window.addEventListener("load", () => {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", event => {
+        let testName = document.querySelector("input[name=testName]");
+        let testDate = document.querySelector("input[name=testDate]");
+        let boosterCount = document.querySelector("input[name=boosterCount]");
+        if (testName.value === "" || testDate.value === "" || boosterCount.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+        }
+        
+    });
+
+});
